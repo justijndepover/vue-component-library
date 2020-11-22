accf4c8fc0d695998b034f3bb2c754ec472be376
Justijn Depover <justijndepover@gmail.com>
2020-11-22 10:42:00 +0100

    loader on page

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -2,6 +2,9 @@
     <div class="flex-1 flex overflow-hidden">
         <div class="p-3 flex-1 overflow-y-auto">
             <slot v-if="!loading"></slot>
+            <div v-if="loading" class="flex h-full items-center justify-center">
+                <img src="/assets/loader.gif" alt="loading" class="w-64">
+            </div>
         </div>
     </div>
 </template>

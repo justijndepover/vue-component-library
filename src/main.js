41eeaf77cba4f18b25c00c56a6769ad7ab410f5d
Justijn Depover <justijndepover@gmail.com>
2020-12-29 15:14:26 +0100

    added pagination component (wip)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import oAlert from './components/Alert';
 import oModal from './components/Modal';
 import oPage from './components/Page';
+import oPagination from './components/Pagination';
 import oToggle from './components/Toggle';
 
-export { oAlert, oModal, oPage, oToggle };
+export { oAlert, oModal, oPage, oPagination, oToggle };

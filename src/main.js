fcb002f79f31131e1109174cacbe64a0f84e3f26
Justijn Depover <justijndepover@gmail.com>
2021-01-03 15:23:44 +0100

    modal

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import oAlert from './components/Alert/Alert';
 import oCard from './components/Card/Card';
 import oCardBody from './components/Card/CardBody';
 import oCardFooter from './components/Card/CardFooter';
-import oModal from './components/Modal';
+import oModal from './components/Modal/Modal';
 import oPage from './components/Page/Page';
 import oPagination from './components/Pagination/Pagination';
 import oToggle from './components/Toggle';

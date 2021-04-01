3374046589931520749d049861a3cc95bb129039
Justijn Depover <justijndepover@gmail.com>
2021-04-01 21:04:58 +0200

    wip

--- src/components/Toggle/Toggle.vue
+++ src/components/Toggle/Toggle.vue
@@ -8,7 +8,7 @@
                 @click="toggle"
                 :aria-pressed="value"
                 :class="checked ? 'bg-blue-300' : 'bg-gray-200'"
-                class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
                 <span class="sr-only">Use setting</span>
                 <span aria-hidden="true" :class="checked ? 'translate-x-5' : 'translate-x-0'" class="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
             </button>

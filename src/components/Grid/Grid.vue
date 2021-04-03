3f7f921bfad84af1c5d3cded9f55a2fbb407ad01
Justijn Depover <justijndepover@gmail.com>
2021-04-03 16:52:28 +0200

    bugfix

--- src/components/Grid/Grid.vue
+++ src/components/Grid/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid gap-6 mb-6" :class="'grid-cols-' + colspan">
+    <div class="grid gap-6 mb-6" :class="className">
         <slot></slot>
     </div>
 </template>
@@ -13,5 +13,28 @@ export default {
             default: 6,
         },
     },
+    computed: {
+        className() {
+            if (this.colspan == 1) {
+                return 'grid-cols-1';
+            }
+            if (this.colspan == 2) {
+                return 'grid-cols-2';
+            }
+            if (this.colspan == 3) {
+                return 'grid-cols-3';
+            }
+            if (this.colspan == 4) {
+                return 'grid-cols-4';
+            }
+            if (this.colspan == 5) {
+                return 'grid-cols-5';
+            }
+            if (this.colspan == 6) {
+                return 'grid-cols-6';
+            }
+            return 'grid-cols-6';
+        }
+    }
 }
 </script>
--- src/components/Grid/GridItem.vue
+++ src/components/Grid/GridItem.vue
@@ -13,5 +13,25 @@ export default {
             default: 6,
         },
     },
+    computed: {
+        className() {
+            if (this.colspan == 1) {
+                return 'md:col-span-1';
+            }
+            if (this.colspan == 2) {
+                return 'md:col-span-2';
+            }
+            if (this.colspan == 3) {
+                return 'md:col-span-3';
+            }
+            if (this.colspan == 4) {
+                return 'md:col-span-4';
+            }
+            if (this.colspan == 5) {
+                return 'md:col-span-5';
+            }
+            return 'md:col-span-6';
+        }
+    }
 }
 </script>
